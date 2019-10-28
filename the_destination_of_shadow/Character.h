fbab7ca8ea452c390662af6b1bc15e1b6b023034
MinoriNakamura <saintlyterritory526@gmail.com>
2019-10-28 16:18:38 +0900

    #5 コンストラクタを利用して、追加したメンバ変数を初期化
     #5 コンストラクタを利用して、追加したメンバ変数を初期化

--- the_destination_of_shadow/Character.h
+++ the_destination_of_shadow/Character.h
@@ -1,14 +1,36 @@
-﻿#include"DirectX.h"
+﻿#ifndef CHARACTER_H
+#define CHARACTER_H
+
+#include"DirectX.h"
+
+enum Direction
+{
+	Right,
+	Left,
+};
 
 class CHARACTER_BASE 
 {
 protected:
-	INT hp;
-	FLOAT width;
-	FLOAT height;
-	FLOAT move_speed;
+	INT m_hp;
+	FLOAT m_move_speed;
+	Direction m_save_direction;
+
+	TEX m_TEX;
+
+	FLOAT m_x;
+	FLOAT m_y;
+	FLOAT m_x_width;
+	FLOAT m_y_height;
+
+	FLOAT m_tu;
+	FLOAT m_tv;
+	FLOAT m_tu_width;
+	FLOAT m_tv_height;
 
 public:
 	virtual VOID Move()=0;
-
 };
+
+#endif
+
--- the_destination_of_shadow/Player.cpp
+++ the_destination_of_shadow/Player.cpp
@@ -16,30 +16,63 @@ VOID Player::Attack()
 	return VOID();
 }
 
-VOID Player::Move()
-{
-	return VOID();
-}
-
 VOID Player::ShadowInstallation()
 {
 	return VOID();
 }
 
-VOID Player::Move(INT* player_x,Player* player)
+
+Player::Player()
 {
+
+	m_hp;
+	m_move_speed = 4.0f;
+	m_save_direction;
+
+	m_TEX = PLAYER;
+	
+	m_x = 0;
+	m_y = 780.0f;
+	m_x_width = 300.0f;
+	m_y_height = 300.0f;
+
+	m_tu = 0.0f;
+	m_tv = 0.0f;
+	m_tu_width = 1.0f;
+	m_tv_height = 1.0f;
+
+}
+
+VOID Player::Move()
+{
+
 	
 	if (directx.XinputState.Gamepad.sThumbLX >= DEAD_ZONE_RIGHT)
 	{
-		*player_x += m_work_speed;
+		m_save_direction = Right;
+		m_x += m_move_speed;
 
 	}
 
 	else if (directx.XinputState.Gamepad.sThumbLX <= -DEAD_ZONE_LEFT)
 	{
-		*player_x -= m_work_speed;
+		m_save_direction = Left;
+		m_x -= m_move_speed;
 
 	}
+
+	switch (m_save_direction)
+	{
+	case Right:
+		m_tu_width = -1.0;
+		break;
+	case Left:
+		m_tu_width = 1.0;
+		break;
+	default:
+		break;
+	}
+
 
 }
 
