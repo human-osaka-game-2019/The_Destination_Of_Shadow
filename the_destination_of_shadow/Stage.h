9889cf021652e0f65f8da80e04147e787be58b6b
MinoriNakamura <saintlyterritory526@gmail.com>
2019-11-18 15:22:47 +0900

    #19 Stage.cpp、hおよびクラスを作成

--- the_destination_of_shadow/Stage.h
+++ the_destination_of_shadow/Stage.h
@@ -1,1 +1,31 @@
-﻿
+﻿#ifndef STAGE_H
+#define STAGE_H
+
+#include "DirectX.h"
+#include "Background.h"
+
+
+class Stage
+{
+public:
+
+	/**
+	* @enum CurrentStage
+	* 現在どのステージにいるかの判定
+	*/
+	enum class CurrentStage
+	{
+		//! 影の世界
+		SHADOW,
+		//! 現実世界
+		REAL
+	};
+	CurrentStage current_stage;
+
+	RealBackground real_background;
+	ShadowBackground shadow_background;
+
+	//! 遷移のクールタイム
+	INT fc_cooldown = 30;
+};
+#endif
