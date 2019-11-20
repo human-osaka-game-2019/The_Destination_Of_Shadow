5d67fe55d5ab9287c20051bdd9cab790e2cab52a
上久保豪太 <kamikubo1123@gmail.com>
2019-11-20 16:28:03 +0900

    #41 cursorクラス作成

---
+++ the_destination_of_shadow/Cursor.cpp
@@ -0,0 +1,42 @@
+﻿#include "Cursor.h"
+
+Cursor::Cursor(XyCoordinate player)
+{
+	xinput = Xinput::GetInstance();
+
+	m_move_speed = 4.0f;
+
+	xy_coordinate.m_x = player.m_x + player.m_width;
+	xy_coordinate.m_y = player.m_y + player.m_height;
+	xy_coordinate.m_width = 100.0f;
+	xy_coordinate.m_height = 100.0f;
+
+	texture.SetTu(0.0f);
+	texture.SetTv(0.0f);
+	texture.SetTuWidth(1.0f);
+	texture.SetTvHeight(1.0f);
+	//texture.SetTextureNum();
+}
+
+VOID Cursor::CursorMove()
+{
+	if (xinput->GetStick(STICK::LEFT_X) >= XINPUT_GAMEPAD_LEFT_THUMB_DEADZONE)
+	{
+		xy_coordinate.m_x += m_move_speed;
+	}
+
+	if (xinput->GetStick(STICK::LEFT_X) <= -XINPUT_GAMEPAD_LEFT_THUMB_DEADZONE)
+	{
+		xy_coordinate.m_x -= m_move_speed;
+	}
+
+	if (xinput->GetStick(STICK::LEFT_Y) >= XINPUT_GAMEPAD_LEFT_THUMB_DEADZONE)
+	{
+		xy_coordinate.m_y -= m_move_speed;
+	}
+
+	if (xinput->GetStick(STICK::LEFT_Y) <= -XINPUT_GAMEPAD_LEFT_THUMB_DEADZONE)
+	{
+		xy_coordinate.m_y += m_move_speed;
+	}
+}
---
+++ the_destination_of_shadow/Cursor.h
@@ -0,0 +1,28 @@
+﻿#ifndef CURSOR_H_
+#define CURSOR_H_
+
+#include "Object.h"
+#include "Xinput.h"
+
+class Cursor : public Object
+{
+private:
+
+	Xinput* xinput;
+
+	FLOAT m_move_speed;
+
+public:
+
+	/**
+	* @brief プレイヤーの前に描画されるように初期化する用
+	*/
+	Cursor(XyCoordinate player);
+	virtual ~Cursor();
+	/**
+	* @brief カーソル移動関数
+	*/
+	VOID CursorMove();
+};
+
+#endif // !CURSOR_H_
