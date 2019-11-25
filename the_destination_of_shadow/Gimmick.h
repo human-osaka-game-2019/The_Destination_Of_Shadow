4f81cb46c11d2d71ef730cd4b8c8c80685c669c0
上久保豪太 <kamikubo1123@gmail.com>
2019-11-25 17:03:52 +0900

    :art: #42 ギミック用クラスのメンバ変数の初期化処理を変更

--- the_destination_of_shadow/Gimmick.h
+++ the_destination_of_shadow/Gimmick.h
@@ -1,19 +1,27 @@
-#ifndef GIMMICK_H_
+﻿#ifndef GIMMICK_H_
 #define GIMMICK_H_
 
 #include "Object.h"
 
-class Gimmick:public Object
+enum class GIMMICK_ID
+{
+	NO_ID,
+	BUILDING,
+};
+
+class Gimmick : public Object
 {
 private:
 
-	BOOL is_shadow = TRUE;
+	GIMMICK_ID m_id = GIMMICK_ID::NO_ID;
+	BOOL m_is_shadow = TRUE;
 
 public:
 
-	Gimmick(FLOAT x,FLOAT y);
+	Gimmick(FLOAT x, FLOAT y, GIMMICK_ID id) {}
 	virtual ~Gimmick() {}
-	VOID MakeTransparent()
+	VOID InitBuilding(FLOAT x, FLOAT y);
+	VOID MakeTransparent();
 };
 
 #endif // !GIMMICK_H_
