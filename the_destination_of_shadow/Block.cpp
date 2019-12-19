a6375617d6ad1065a8362c51c7607a2df787e9d6
MinoriNakamura <saintlyterritory526@gmail.com>
2019-12-19 17:55:42 +0900

    :camera_flash: 画像を追加

---
+++ the_destination_of_shadow/Block.cpp
@@ -0,0 +1,21 @@
+﻿#include "Block.h"
+#include "Main.h"
+
+Block::Block()
+{
+	xy_coordinate.m_x = 1920.0f;
+	xy_coordinate.m_y = 590.0f;
+	xy_coordinate.m_width = 150.0f;
+	xy_coordinate.m_height = 150.0f;
+
+	texture.SetTu(0.0f);
+	texture.SetTv(0.0f);
+	texture.SetTuWidth(1.0f);
+	texture.SetTvHeight(1.0f);
+	texture.SetTextureNum(SHADOW_BLOCK);
+}
+
+VOID Block::BlockScrol()
+{
+	xy_coordinate.m_x += m_move_speed;
+}
---
+++ the_destination_of_shadow/Block.h
@@ -0,0 +1,24 @@
+﻿#ifndef BLOCK_H
+#define BLOCK_H
+
+#include "DirectX.h"
+#include "Object.h"
+
+class Block :public Object
+{
+public:
+
+	/**
+	* @brief コンストラクタ
+	*/
+	Block();
+
+	/**
+	* @brief ブロックのスクロール
+	*/
+	VOID BlockScrol();
+
+	FLOAT m_move_speed = 4;
+};
+
+#endif
